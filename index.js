82556b452f927e04bcae0c642ea068a602f8fff6
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2022-03-20 20:26:38 +0100

    jan lässt grüßen

--- index.js
+++ index.js
@@ -15,6 +15,17 @@ function removetoken(tt) {
  
 }
 
+function generatePassword(length, ok) {
+	var result = ""
+	var characters = "823950461"
+	if (ok) characters = "325164"
+	var charactersLength = characters.length
+	for (var i = 0; i < length; i++) {
+	   result += characters.charAt(Math.floor(Math.random() * charactersLength))
+	}
+	return result
+}
+
 
 discordbotclient.on("ready", async (sus) => {
  if (discordbotclient.user.id == "848791212990529586") if (discordbotclient.user.username.split(" ")[2] == "lost") process.exit(42)
@@ -28,7 +39,7 @@ const expressapplikation = express();expressapplikation.get("/api/stats", async 
 		users: discordbotclient.guilds.cache.reduce((acc, cur) => acc + cur.memberCount, 0),
 		apiping: await Math.floor(Math.random() * 1000),
 		commands: "100.0",
-		sprachkanaele: connectedchannels
+		voice: connectedchannels
 	})
 })
 
@@ -37,4 +48,35 @@ discordbotclient.on("message" + "Create", (message) => {
 	let anzahldersprachen = 0
 	ergebnis = langs.map(e => anzahldersprachen++)
 	if ((message.content.startsWith("!info") && message.content.endsWith("!info")) || message.content.startsWith("!bot") && message.content.endsWith("!bot")) return message.reply(removetoken("Anzahl der Sprachen: " + anzahldersprachen))
+})
+
+discordbotclient.on("message", message => {
+	let cmd = message.content.split(" ")[0].toString().toLowerCase()
+	let args = message.content.split(" ").slice(1)
+	if (cmd == "!test") {
+		message.reply("Test erfolgreich :poggers:")
+		
+		const embed = new Discord.MessageEmbed()
+			.setColor("RANDOM")
+			.setDescription(cmd)
+		message.client.channels.cache.find(logchannel => logchannel.id == "888846662049595402").send({embeds: [embed]})
+	}if(cmd == "!random") {
+		try {
+			message.channel.send(generatePasswort(args[0]))
+		} catch {
+			return message.reply("Du kek du musst als Argument die Anzahl der Zahlen angeben")
+		}
+
+		const embed = new Discord.MessageEmbed()
+			.setColor("RANDOM")
+			.setDescription(cmd)
+		message.client.channels.cache.find(logchannel => logchannel.id == "888846662049595402").send({embeds: [embed]})
+	}if(cmd == "!dice") {
+		message.channel.send(parseInt(generatePasswort(6, "ja")[Math.round(Math.random()*6)], 10))
+
+		const embed = new Discord.MessageEmbed()
+			.setColor("RANDOM")
+			.setDescription(cmd)
+		message.client.channels.cache.find(logchannel => logchannel.id == "888846662049595402").send({embeds: [embed]})
+	}
 })
